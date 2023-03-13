c80404f92f910cd6990559b7ad7f2b0da7ef9ada
FDSAP-GTan <g.tan@FDSAP-HARVEY.local>
2023-03-13 13:53:33 +0800

    first commit

---
+++ pkg/utils/go-utils/database/queryWarehouse.go
@@ -0,0 +1,11 @@
+package database
+
+// PUT YOUR DATABASE SCRIPT HERE
+// CREATE TABLE accounts (
+// 	user_id serial PRIMARY KEY,
+// 	username VARCHAR ( 50 ) UNIQUE NOT NULL,
+// 	password VARCHAR ( 50 ) NOT NULL,
+// 	email VARCHAR ( 255 ) UNIQUE,
+// 	contact VARCHAR ( 50 ) UNIQUE,
+// 	created_on TIMESTAMP NOT NULL
+// );
